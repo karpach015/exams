7290d7ca7304d0fc6320317dc7ee58c6f1e9251b
karpach015 <46755740+karpach015@users.noreply.github.com>
2021-06-16 16:46:31 +0300

    V4

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ first_time_dict = {
     "Võru": None
 }
 # before_date = datetime.now() + timedelta(days=365)
-before_date = datetime.strptime("26.01.21", "%d.%m.%y")
+before_date = datetime.strptime("26.06.21", "%d.%m.%y")
 
 
 @dp.callback_query_handler(callback.search_settings.filter())
